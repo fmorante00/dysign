057234b05dc5ac40d801abcf8ce7a8aa3be0d60c
Francis Morante <fmorante00@gmail.com>
2026-09-17 18:07:50 +0800

    Setup authentication and role middleware

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$roles): Response
+{
+    if (!auth()->check()) {
+        abort(401);
+    }
+
+    if (!$request->user()->role) {
+        abort(403);
+    }
+
+    if (!in_array($request->user()->role->role_name, $roles)) {
+        abort(403);
+    }
+
+    return $next($request);
+}
+}
